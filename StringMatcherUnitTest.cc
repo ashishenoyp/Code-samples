e9011a804beeee800a59496c211bcdc2df6077bc
ashishenoyp <ashishenoyp@gmail.com>
2014-10-08 13:54:11 -0700

    Create StringMatcherUnitTest.cc

---
+++ StringMatcherUnitTest.cc
@@ -0,0 +1,33 @@
+#include "StringMatcher.h"
+#include <stdio.h>
+#include <assert.h>
+
+int
+main(int argc, char **argv)
+{
+    // Basic exact match
+    {
+        StringMatcher matcher;
+        matcher.add_exact_match("contactus", 1);
+        assert(matcher.lookup("contactus") == 1);
+    }
+
+    // Basic prefix test
+    {
+        StringMatcher matcher;
+        matcher.add_prefix_match("img", 1);
+        assert(matcher.lookup("imgcutepuppy") == 1);
+        assert(matcher.lookup("htmlcutepuppy") == -1);
+    }
+
+    // Basic longest match test
+    {
+        StringMatcher matcher;
+        matcher.add_prefix_match("img", 1);
+        matcher.add_prefix_match("imghd", 2);
+        assert(matcher.lookup("imgcutepuppy") == 1);
+        assert(matcher.lookup("imghdcutepuppy") == 2);
+    }
+
+    return 0;
+}
